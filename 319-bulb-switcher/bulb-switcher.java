635c702951560b1336438be20eaf9345cba1a3ab
NikhilRajput23 <nikhilrajput3006@gmail.com>
2025-06-24 22:48:36 +0530

    Time: 52 ms (2.97%) | Memory: 44.2 MB (5.05%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.java
+++ 319-bulb-switcher/bulb-switcher.java
@@ -1,6 +1,6 @@
 class Solution {
     public int bulbSwitch(int n) {
-        for(int i=0;i*i<=n;i++){
+        for(int i=1;i*i<=n;i++){
            System.out.println(i);
         }
         return (int)Math.sqrt(n);
