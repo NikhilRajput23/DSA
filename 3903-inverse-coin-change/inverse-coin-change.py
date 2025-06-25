cb24cb1a41f6d8cb2821bb2ce830f95bd80be49f
NikhilRajput23 <nikhilrajput3006@gmail.com>
2025-06-25 19:58:37 +0530

    Time: 4 ms (94.58%) | Memory: 12.4 MB (89.16%) - LeetSync

---
+++ 3903-inverse-coin-change/inverse-coin-change.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def findCoins(self, numWays):
+        N = len(numWays)
+        dp = [0] * (N + 1)
+        dp[0] = 1
+        
+        coins = []
+        for i in range(1, N + 1):
+            target = numWays[i - 1]
+        
+            if dp[i] == target - 1:
+                coins.append(i)
+                for j in range(i, N + 1):
+                    dp[j] += dp[j - i]
+            if dp[i] != target:
+                return []
+        
+        return coins
