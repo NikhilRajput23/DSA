69297954f8133325cb9c812e9a5740f8afc85839
NikhilRajput23 <nikhilrajput3006@gmail.com>
2025-06-25 19:51:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (53.70%) - LeetSync

---
+++ 672-bulb-switcher-ii/bulb-switcher-ii.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int flipLights(int n, int presses) {
+        if (presses == 0) return 1;
+        if (n == 1) return 2;
+        if (n == 2) {
+            if (presses == 1) return 3;
+            else return 4;
+        }
+        if (presses == 1) return 4;
+        else if (presses == 2) return 7;
+        else return 8;
+    }
+}
