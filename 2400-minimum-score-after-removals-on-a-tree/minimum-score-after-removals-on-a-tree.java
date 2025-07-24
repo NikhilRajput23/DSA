525363ef63c8af57ecfe8de285a2533aa4f0e517
NikhilRajput23 <nikhilrajput3006@gmail.com>
2025-07-24 21:30:00 +0530

    Time: 187 ms (48.28%) | Memory: 45.5 MB (24.14%) - LeetSync

--- 2400-minimum-score-after-removals-on-a-tree/minimum-score-after-removals-on-a-tree.java
+++ 2400-minimum-score-after-removals-on-a-tree/minimum-score-after-removals-on-a-tree.java
@@ -1,72 +1,52 @@
 class Solution {
-
+    int res = Integer.MAX_VALUE;
     public int minimumScore(int[] nums, int[][] edges) {
         int n = nums.length;
-        List<List<Integer>> adj = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0;i<n;i++){
+            list.add(new ArrayList<>());
         }
-        for (int[] e : edges) {
-            adj.get(e[0]).add(e[1]);
-            adj.get(e[1]).add(e[0]);
+        for(int[] e:edges){
+            list.get(e[0]).add(e[1]);
+            list.get(e[1]).add(e[0]);
         }
-
-        int[] sum = new int[n];
-        int[] in = new int[n];
-        int[] out = new int[n];
-        int[] cnt = { 0 };
-
-        dfs(0, -1, nums, adj, sum, in, out, cnt);
-        int res = Integer.MAX_VALUE;
-        for (int u = 1; u < n; u++) {
-            for (int v = u + 1; v < n; v++) {
-                if (in[v] > in[u] && in[v] < out[u]) {
-                    res = Math.min(
-                        res,
-                        calc(sum[0] ^ sum[u], sum[u] ^ sum[v], sum[v])
-                    );
-                } else if (in[u] > in[v] && in[u] < out[v]) {
-                    res = Math.min(
-                        res,
-                        calc(sum[0] ^ sum[v], sum[v] ^ sum[u], sum[u])
-                    );
-                } else {
-                    res = Math.min(
-                        res,
-                        calc(sum[0] ^ sum[u] ^ sum[v], sum[u], sum[v])
-                    );
-                }
-            }
+        int sum = 0;
+        for(int num:nums){
+            sum ^= num;
         }
+        dfs(0,-1,nums,list,sum);
         return res;
     }
-
-    private int calc(int part1, int part2, int part3) {
-        return (
-            Math.max(part1, Math.max(part2, part3)) -
-            Math.min(part1, Math.min(part2, part3))
-        );
-    }
-
-    private void dfs(
-        int x,
-        int fa,
-        int[] nums,
-        List<List<Integer>> adj,
-        int[] sum,
-        int[] in,
-        int[] out,
-        int[] cnt
-    ) {
-        in[x] = cnt[0]++;
-        sum[x] = nums[x];
-        for (int y : adj.get(x)) {
-            if (y == fa) {
+    private int dfs(int x,int f,int[] nums,List<List<Integer>> list,int sum){
+        int s = nums[x];
+        for(int y:list.get(x)){
+            if(y == f){
                 continue;
             }
-            dfs(y, x, nums, adj, sum, in, out, cnt);
-            sum[x] ^= sum[y];
+            s ^= dfs(y,x,nums,list,sum);
         }
-        out[x] = cnt[0];
+        for(int y:list.get(x)){
+            if(y == f){
+                dfs2(y,x,s,x,nums,list,sum);
+            }
+        }
+        return s;
+    }
+    private int dfs2(int x,int f,int oth,int anc,int[] nums,List<List<Integer>> list,int sum){
+        int s = nums[x];
+        for(int y:list.get(x)){
+            if(y == f){
+                continue;
+            }
+            s ^= dfs2(y,x,oth,anc,nums,list,sum);
+        }
+        if(f == anc){
+            return s;
+        }
+        res = Math.min(res,calc(oth,s,sum^oth^s));
+        return s;
+    }
+    private int calc(int part1,int part2,int part3){
+        return (Math.max(part1,Math.max(part2,part3))-Math.min(part1,Math.min(part2,part3)));
     }
 }
