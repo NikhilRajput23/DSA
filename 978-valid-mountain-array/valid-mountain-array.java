334599efb24d7413afee4a733a669a1c288c0379
NikhilRajput23 <nikhilrajput3006@gmail.com>
2025-10-04 20:49:30 +0530

    Time: 3 ms (14.39%) | Memory: 46 MB (7.92%) - LeetSync

---
+++ 978-valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validMountainArray(int[] arr) {
+        int peak=0;
+    if(arr.length<3){
+        return false;
+    }
+    for(int i =1;i<arr.length-1;i++){
+        int prev=arr[i-1];
+        int curr=arr[i];
+        int next=arr[i+1];
+        if(prev==curr || curr==next){
+            return false;
+        }
+        if(prev>curr && curr<next){
+            return false;
+        }
+        if(prev<curr && curr>next){
+            peak++;
+            if(peak>1){
+                return false;
+            }
+        }
+    }
+    return peak==1;
+    }
+}
