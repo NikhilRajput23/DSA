2571c3254b2d372a33d4832973af2e01882ab89c
NikhilRajput23 <nikhilrajput3006@gmail.com>
2025-10-22 17:36:24 +0530

    Time: 196 ms (30.46%) | Memory: 72.4 MB (20.53%) - LeetSync

--- 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/maximum-frequency-of-an-element-after-performing-operations-i.java
@@ -1,23 +1,63 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
-    public int maxFrequency(int[] nums, int k, int numOps) {
-        int maxVal = Arrays.stream(nums).max().getAsInt() + k + 2;
-        int[] count = new int[maxVal];
-
-        for (int v : nums)
-            count[v]++;
-
-        for (int i = 1; i < maxVal; i++)
-            count[i] += count[i - 1];
-
-        int res = 0;
-        for (int i = 0; i < maxVal; i++) {
-            int left = Math.max(0, i - k);
-            int right = Math.min(maxVal - 1, i + k);
-            int total = count[right] - (left > 0 ? count[left - 1] : 0);
-            int freq = count[i] - (i > 0 ? count[i - 1] : 0);
-            res = Math.max(res, freq + Math.min(numOps, total - freq));
+    public int first(int[] a, int t) {
+        int l = 0, r = a.length - 1;
+        int m, idx = a.length;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (a[m] < t) {
+                l = m + 1;
+            } else {
+                idx = Math.min(m, idx);
+                r = m - 1;
+            }
         }
+        return idx;
+    }
 
-        return res;
+    public int last(int[] a, int t) {
+        int l = 0, r = a.length - 1;
+        int m, idx = a.length;
+        while (l <= r) {
+            m = (l + r) / 2;
+            if (a[m] > t) {
+                r = m - 1;
+            } else {
+                idx = m;
+                l = m + 1;
+            }
+        }
+        return idx;
+    }
+
+    public int maxFrequency(int[] nums, int k, int numOperations) {
+        int max = 0;
+        Map<Integer, Integer> m1 = new HashMap<>();
+        Map<Integer, Integer> m2 = new HashMap<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; ++i) {
+            m1.put(nums[i], m1.getOrDefault(nums[i], 0) + 1);
+            m2.putIfAbsent(nums[i], i);
+        }
+        var keys = m1.keySet();
+        int cnt, f, l;
+        for (var key : keys) {
+            cnt = m1.get(key);
+            f = first(nums, key - k);
+            l = last(nums, key + k);
+            if (f != nums.length && l != nums.length) {
+                cnt += Math.min(l - f - cnt + 1, numOperations);
+                max = Math.max(max, cnt);
+            }
+            l = last(nums, key + k + k);
+            if (l != nums.length) {
+
+                max = Math.max(max, Math.min(l - m2.get(key) + 1, numOperations));
+            }
+        }
+        return max;
     }
 }
