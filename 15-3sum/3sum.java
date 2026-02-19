e703144521c250d33b69f55a7c41a22672215b01
Nikhil Rajput <nikhilrajput3006@gmail.com>
2026-02-19 13:59:14 +0530

    Time: 35 ms (44.33%) | Memory: 59 MB (86.08%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -2,29 +2,39 @@ import java.util.*;
 
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Set<List<Integer>> result = new HashSet<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            int target = -nums[i];
-            HashMap<Integer, Integer> a= new HashMap<>();
-
-            for (int j = i + 1; j < nums.length; j++) {
-                int complement = target - nums[j];
-
-if (a.containsKey(complement)) {
-         int n = nums[i];
-          int m = nums[j]; 
-          int k = complement;
-     int[] t = new int[]{n, m, k};
-     Arrays.sort(t); 
-
-      result.add(Arrays.asList(t[0], t[1], t[2]));
+        
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        
+        for(int i = 0; i < nums.length - 2; i++) {
+            
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            
+            int left = i + 1;
+            int right = nums.length - 1;
+            
+            while(left < right) {
+                
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if(sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    
+                    while(left < right && nums[left] == nums[left+1]) left++;
+                    while(left < right && nums[right] == nums[right-1]) right--;
+                    
+                    left++;
+                    right--;
                 }
-
-               a.put(nums[j], j);
+                else if(sum < 0) {
+                    left++;
+                }
+                else {
+                    right--;
+                }
             }
         }
-
-        return new ArrayList<>(result);
+        
+        return result;
     }
 }
